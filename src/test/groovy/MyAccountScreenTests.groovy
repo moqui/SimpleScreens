9344669bfee8391e064b1ea60c841d2a63aa4949
David E Jones <dej@dejc.com>
2017-02-02 15:48:34 -0800

    Cleanups, fixed issue with MyTasks create task not assigning to current user after underlying service change

--- src/test/groovy/MyAccountScreenTests.groovy
+++ src/test/groovy/MyAccountScreenTests.groovy
@@ -104,7 +104,7 @@ class MyAccountScreenTests extends Specification {
         "User/Account" | ['worker@test.com', 'Worker', 'Change Password']
 
         "Expense/FindExpenseInvoice" | ['Test Vendor', 'Billed Through']
-        "Expense/EditExpenseInvoice?invoiceId=55900" | ['Test Worker', 'Billed Through', '849.12']
+        "Expense/EditExpenseInvoice?invoiceId=55900" | ['Test Worker', 'Billed Through', '1009.12']
         "Expense/EditExpenseInvoiceItems?invoiceId=55900" |
                 ['Expense - Travel Air', 'United SFO-LAX', 'Sales - Time Entry', '240.00']
     }
